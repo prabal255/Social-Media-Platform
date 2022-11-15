cc2973c662618cf75dabffc3aca6f02073b93953
Shivanisaini12 <112395473+Shivanisaini12@users.noreply.github.com>
2022-11-15 12:39:21 +0530

    Added userprofile changes

--- socialMediaPlatform/src/app/user-management/user-management-routing.module.ts
+++ socialMediaPlatform/src/app/user-management/user-management-routing.module.ts
@@ -1,11 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomePageComponent } from './home-page/home-page.component';
+import { UserProfileComponent } from './user-profile/user-profile.component';
 
 const routes: Routes = [
   {
     path:'',
     component:HomePageComponent
+  },
+  {
+    path:'profile',
+    component:UserProfileComponent
   }
 ];
 
