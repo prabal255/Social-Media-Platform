a31e691a862dd8233f37cb0d158b604f3ec196cc
prabal255 <2017pcecsprabal115@poornima.org>
2022-11-15 10:19:50 +0530

    Added components in userManagement

--- socialMediaPlatform/src/app/user-management/user-management.module.ts
+++ socialMediaPlatform/src/app/user-management/user-management.module.ts
@@ -5,13 +5,19 @@ import { UserManagementRoutingModule } from './user-management-routing.module';
 import { UserSignInComponent } from './user-sign-in/user-sign-in.component';
 import { UserSignUpComponent } from './user-sign-up/user-sign-up.component';
 import { UserManagementComponent } from './user-management.component';
+import { HomePageComponent } from './home-page/home-page.component';
+import { UserProfileComponent } from './user-profile/user-profile.component';
+import { ChangePasswordComponent } from './change-password/change-password.component';
 
 
 @NgModule({
   declarations: [
     UserSignInComponent,
     UserSignUpComponent,
-    UserManagementComponent
+    UserManagementComponent,
+    HomePageComponent,
+    UserProfileComponent,
+    ChangePasswordComponent
   ],
   imports: [
     CommonModule,
