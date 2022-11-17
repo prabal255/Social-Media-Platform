686e070b8bc47f6900eb4437e3669863911ab882
prabal255 <2017pcecsprabal115@poornima.org>
2022-11-17 12:25:26 +0530

    Added Hamburger

--- socialMediaPlatform/src/app/Views/User/Modules/user-panel/user-panel.component.ts
+++ socialMediaPlatform/src/app/Views/User/Modules/user-panel/user-panel.component.ts
@@ -12,5 +12,20 @@ export class UserPanelComponent implements OnInit {
 
   ngOnInit(): void {
   }
+  class="res-menu mm-menu mm-offcanvas"
+  forOpened=""
 
+
+menu(){
+  debugger
+  if(this.class=="res-menu mm-menu mm-offcanvas")
+  {
+    this.class="res-menu mm-menu mm-offcanvas mm-opened"
+    this.forOpened="mm-opened mm-blocking mm-background mm-opening"
+  }
+  else{
+    this.class="res-menu mm-menu mm-offcanvas"
+    this.forOpened=""
+  }
+}
 }
